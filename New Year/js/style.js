df1bbaf23d0227c987ff1d7f82f36c3e8cf1ff0a
Lee-Xiaocai <46062457+Lee-Xiaocai@users.noreply.github.com>
2019-02-22 14:52:37 +0800

    Add files via upload

---
+++ New Year/js/style.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+window.onload=function(){
+	var page1=document.getElementById("page1");
+	var page2=document.getElementById("page2");
+	var page3=document.getElementById("page3");
+	
+	var music=document.getElementById("music");
+	var audio=document.getElementsByTagName("audio")[0];
+	
+	
+
+	
+	//音乐播放器
+	audio.addEventListener("ended",function(){
+		music.setAttribute("class","");
+	},false);
+	
+	music.addEventListener("touchstart",function(){
+		if(audio.paused){
+			audio.play();
+		    this.setAttribute("class","play");
+		}else{
+			audio.pause();
+			this.setAttribute("class","");
+		}		
+	},false);
+	
+	//翻页
+
+    page1.addEventListener("touchstart", function() {
+        this.style.display = "none";
+        page2.style.display = "block";
+
+        setTimeout(function() {
+//            page2.setAttribute("class","page fadeOut");
+//            page3.setAttribute("class","page fadeIn");
+            page2.style.display = "none";
+            page3.style.display = "block";
+        }, 6000);
+    }, false);
+};
